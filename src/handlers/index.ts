63b84b3061d124927883dbf671a180e3e2a31e72
abellan93 <raul.abellan.dev@gmail.com>
2025-01-14 10:25:45 +0100

    auth-hash y slug

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,16 +1,28 @@
 import User from "../models/User.js";
+import { hashPassword } from "../utils/auth.js";
+import slug from "slug";
 
 export const createAcount = async (req, res) => {
 
-    const {email} = req.body;
+    const { email, password } = req.body;
 
-    const emailExist = await User.findOne({email});
-    if(emailExist){
-       const error = new Error('El email ya está en uso');
-        return res.status(409).json({error: error.message }) ;
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+        const error = new Error('El email ya está en uso');
+        return res.status(409).json({ error: error.message });
+    }
+
+    const handle = slug(req.body.handle);
+    const handleExist = await User.findOne({ handle });
+    if (handleExist) {
+        const error = new Error('El nombre de usuario no está disponible');
+        return res.status(409).json({ error: error.message });
     }
 
     const user = new User(req.body);
+    user.password= await hashPassword(password);
+    user.handle = handle;
+
     await user.save();
-    res.status(201).send(user.name +' se registro correctamente');
- }
+    res.status(201).send(user.name + ' se registro correctamente');
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,20 +1,36 @@
 import mongoose from 'mongoose';
 interface IUser {
+    handle: string;
     name: string;
     email: string;
     password: string;
 }
 const UserSchema = new mongoose.Schema({
-    name: { type: String,
-         required: true,
-          trim: true },
-    email: { type: String,
-         required: true,
-          trim: true,
-          unique: true },
-    password: { type: String,
-         required: true,
-          trim: true },
+    handle: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
 });
 
 const User = mongoose.model<IUser>('User', UserSchema);
